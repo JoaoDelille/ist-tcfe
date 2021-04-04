efe3bbc207b2a2bdabb9830e96add57970df70f2
JoaoDelille <joao.delille@tecnico.ulisboa.pt>
2021-04-04 23:28:07 +0100

    Ficam so os mats que importam

--- t2/mat/forced_solution.m
+++ t2/mat/forced_solution.m
@@ -1,5 +1,3 @@
-graphics_toolkit('gnuplot')
-
 R1 = 1.02166163214 
 R2 = 2.05713464724 
 R3 = 3.10288701080 
@@ -11,6 +9,26 @@ Vs = 5.24841080700
 C  = 1.00334118042 
 Kb = 7.24529394385 
 Kd = 8.29202733390 
+
+V8=-2.9614
+V6=5.8022
+   %     V2              V3        V5     V6     V7         V8
+A=[  1/R1           , 0       , 1/R4    , 0 , 1/R6       , 0     ;
+    -1/R2-Kb        , 1/R2    , Kb      , 0 , 0          , 0     ;
+     1/R1+1/R2+1/R3 , -1/R2   , -1/R3   , 0 , 0          , 0     ;
+     0              , 0       , 0       , 0 , 1/R6+1/R7  , -1/R7 ;
+     0              , 0       , 0       , 1 , 0          , -1    ;
+     0              , 0       , 1       , 0 , Kd/R6      , -1    ]
+
+y=[0, 0, 0, 0,8.763595,0]
+res=y/A'
+
+Ix=-(V6-res(3))/R5-Kb*(res(1)-res(3))
+Req=(V8-V6)/Ix
+timecte=Req*C
+
+fprintf ( fopen("node_alinea2.tex", "w") , ' V(2) & V(3) & V(5) &  V(7) & Ix & Req & timecte\\\\ \n %g V  &  %g V   &  %g V  &  %g V  &  %g A  &  %g Ohm &  %g  s  \\\\\n' , res(1),res(2),res(3),res(4), Ix, Req, timecte')
+
 
 f=1000
 
@@ -27,12 +45,11 @@ A=[1     , 0               , 0    , 0           , 0         , 0          , 0    
    0     , Kb              , 0    , -1/R5-Kb    , 1/R5+inZc , 0          , -inZc ;
    0     , -1/R2-Kb        , 1/R2 , Kb          , 0         , 0          , 0     ;
    -1/R1 , 1/R1            , 0    , 1/R4        , 0         , 1/R6       , 0     ]
-   
-   
-   y=[vs ; 0 ; 0 ; 0 ; 0 ; 0 ; 0]
 
+y=[vs ; 0 ; 0 ; 0 ; 0 ; 0 ; 0]
 res=y'/A'
 
+fprintf ( fopen("nodal.tex", "w") , '\n V(1) & V(2) & V(3) & V(4) & V(5) & V(6) & V(7) \\\\ \n %g V   & %g V  & %g V  & %g V  & %g V  & %g V  & %g V\\\\\n' , res(1) , res(2) , res(3) , res(4) , res(5) , res(6) , res(7) )
 
 t=0:0.00001:0.02;
 func= real(e.^(2.*pi.*i.*t.*f).*res(5));
@@ -46,7 +63,7 @@ title ("teste da coisa");
 
 print (figura, "yay", "-dsvg");
 
-func2= real(e.^(2.*pi.*i.*t.*f).*res(5))+8.7*e.^(-t/(4.5640*10^(-3)));
+func2= real(e.^(2.*pi.*i.*t.*f).*res(5))+8.7*e.^(-t/(timecte.*10.^-3));
 hold on;
 figura = figure();
 plot ( t , func2 );
@@ -57,20 +74,24 @@ title ("teste da coisa");
 
 print (figura, "yay", "-dsvg"); 
 
-t=0.01;
-f=0.1:100:1000000;
-func3= 20*log10(abs(e.^(2.*pi.*i.*t.*f).*res(5)));
-func4= 20*log10(abs(e.^(2.*pi.*i.*t.*f).*(res(5)-res(7)))); 
-func5= 20*log10(abs(e.^(2.*pi.*i.*t.*f).*(vs)));
-hold on;
-figura = figure();
-plot ( f , func3, f, func4, f , func5);
 
-xlabel ("f");
-ylabel ("func4");
-title ("teste da coisa");
 
-print (figura, "yyyjyy", "-dsvg"); 
+
+
+%t=0.01;
+%f=0.1:100:1000000;
+%func3= 20*log10(abs(e.^(2.*pi.*i.*t.*f).*res(5)));
+%func4= 20*log10(abs(e.^(2.*pi.*i.*t.*f).*(res(5)-res(7)))); 
+%func5= 20*log10(abs(e.^(2.*pi.*i.*t.*f).*(vs)));
+%hold on;
+%figura = figure();
+%plot ( f , func3, f, func4, f , func5);
+
+%xlabel ("f");
+%ylabel ("func4");
+%title ("teste da coisa");
+
+%print (figura, "yyyjyy", "-dsvg"); 
 
 %t=0.01;
 %f=logspace(0.1,1e6,1e4);
--- t2/mat/nodal.m
+++ t2/mat/nodal.m
@@ -20,11 +20,15 @@ A=[1     , 0               , 0    , 0           , 0    , 0          , 0    ;
    0     , Kb              , 0    , -1/R5-Kb    , 1/R5 , 0          , 0    ;
    0     , -1/R2-Kb        , 1/R2 , Kb          , 0    , 0          , 0    ;
    -1/R1 , 1/R1            , 0    , 1/R4        , 0    , 1/R6       , 0    ]
-   
+
 y=[Vs ; 0 ; 0 ; 0 ; Id ; 0 ; 0]
 
 res=y'/A'
 
+fprintf ( fopen("nodeVolt.tex", "w") , '\n V(1) & V(2) & V(3) & V(5) & V(6) & V(7) & V(8) \\\\ \n %g V   & %g V  & %g V  & %g V  & %g V  & %g V  & %g V\\\\\n' , res(1) , res(2) , res(3) , res(4) , res(5) , res(6) , res(7) )
+printf ("\nSuccess!\n")
 
-fprintf ( fopen("nodal.tex", "w") , '\n V(1) & V(2) & V(3) & V(4) & V(5) & V(6) & V(7) \\\\ \n %g V   & %g V  & %g V  & %g V  & %g V  & %g V  & %g V\\\\\n' , res(1) , res(2) , res(3) , res(4) , res(5) , res(6) , res(7) )
+corr=[(res(2)-res(1))/R1 , (res(3)-res(2))/R2 , (res(2)-res(4))/R3 , res(4)/R4 , (res(5)-res(4))/R5 , res(6)/R6 , (res(7)-res(6))/R7 ] 
+
+fprintf ( fopen("nodeCurr.tex", "w") , '\n I(1) & I(2) & I(3) & I(5) & I(6) & I(7) & I(8) \\\\ \n %g A   & %g A  & %g A  & %g A  & %g A  & %g A  & %g A\\\\\n' , corr(1) , corr(2) , corr(3) , corr(4) , corr(5) , corr(6) , corr(7) )
 printf ("\nSuccess!\n")
