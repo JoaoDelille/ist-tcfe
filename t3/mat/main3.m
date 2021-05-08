8d6249161f9774c804699f27cec1448dad2ba0d3
JoaoDelille <joao.delille@tecnico.ulisboa.pt>
2021-05-08 14:09:04 +0100

    More main3

--- t3/mat/main3.m
+++ t3/mat/main3.m
@@ -85,7 +85,7 @@ for k = 1:numel(intervalo)
   
 endfor
 
-ripple=real(max(V3)-min(V3))
+ripple=abs(real(max(V3)-min(V3)))
 V_DC= max(abs(V3)) 
 
 fprintf ( fopen("Oct_results.tex", "w") , '\n V_ripple & V_DC \\\\ \n %g V   & %g V\\\\\n' , ripple , V_DC )
